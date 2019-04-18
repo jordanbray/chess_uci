f517e384eaa4d4ea8d92ce7b502a30a1b298c59f
Jordan Bray <jordanbray@gmail.com>
2019-04-17 22:10:02 -0400

    Added functions needed for search.

--- src/engine_base/eval.rs
+++ src/engine_base/eval.rs
@@ -5,9 +5,10 @@ use std::fmt::Debug;
 
 pub trait Eval: Num + NumAssign + NumAssignOps + NumOps + Signed + Bounded + PartialEq + PartialOrd + NumCast + Debug + Copy  {
     fn max_supported_mates() -> Self;
-    fn from_ply(ply: u16) -> Self;
-    fn new_mate(ply: u16, color: Color) -> Self;
+    fn from_ply(ply: i16) -> Self;
+    fn new_mate(ply: i16, color: Color) -> Self;
     fn depth_to_mate(&self) -> Option<i64>;
+    fn add_depth(&self, amount: i16) -> Self;
 }
 
 impl<T> Eval for T
@@ -17,14 +18,26 @@ where
         Self::from(800).expect("Failed to convert value 800 to Eval type.")
     }
 
-    fn from_ply(ply: u16) -> Self {
+    fn from_ply(ply: i16) -> Self {
         Self::from(ply).expect("Failed to convert u16 ply into Eval type.")
     }
 
-    fn new_mate(ply: u16, color: Color) -> Self {
+    fn new_mate(ply: i16, color: Color) -> Self {
         match color {
-            Color::White => Self::max_value() - (Self::from_ply(ply)),
-            Color::Black => Self::min_value() + (Self::from_ply(ply)),
+            Color::White => Self::max_value() - Self::from_ply(ply),
+            Color::Black => Self::min_value() + Self::from_ply(ply) + Self::one(),
+        }
+    }
+
+    fn add_depth(&self, amount: i16) -> Self {
+        if let Some(depth) = self.depth_to_mate() {
+            if depth < 0 {
+                Self::new_mate(((-depth) as i16) + amount, Color::Black)
+            } else {
+                Self::new_mate((depth as i16) + amount, Color::White)
+            }
+        } else {
+            *self
         }
     }
 
@@ -32,7 +45,7 @@ where
         if *self > Self::max_value() - Self::max_supported_mates() {
             Some((Self::max_value() - *self).to_i64().expect("Failed to convert mate depth to i64"))
         } else if *self < Self::min_value() + Self::max_supported_mates() {
-            Some((Self::min_value() - *self).to_i64().expect("Failed to convert mate depth to i64"))
+            Some((Self::min_value() - *self + Self::one()).to_i64().expect("Failed to convert mate depth to i64"))
         } else {
             None
         }
@@ -41,7 +54,7 @@ where
 
 #[cfg(test)]
 fn test_mates<T: Eval>() {
-    assert_eq!(T::min_value(), T::new_mate(0, Color::Black));
+    assert_eq!(T::min_value() + T::one(), T::new_mate(0, Color::Black));
     assert_eq!(T::max_value(), T::new_mate(0, Color::White));
 
     assert!(T::new_mate(0, Color::Black) < T::new_mate(1, Color::Black));
@@ -52,6 +65,8 @@ fn test_mates<T: Eval>() {
 
     assert_eq!(T::new_mate(10, Color::White).depth_to_mate(), Some(10));
     assert_eq!(T::new_mate(10, Color::Black).depth_to_mate(), Some(-10));
+
+    assert_eq!(-T::new_mate(10, Color::Black), T::new_mate(10, Color::White));
 }
 
 
